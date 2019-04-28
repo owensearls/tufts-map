2c6c14a0f7a445d38910fd98d348a832f6fdad86
Owen Searls <owen@searls.net>
2019-04-28 14:40:30 -0400

    Update data and make map bigger (#2)
    * Switched to d3 CDN
    
    * Upgrade to d3 v5
    
    * Made height auto scaling
    
    * Moved javascript to new file
    
    * Updated ownership tests
    
    * Small refactoring and updated README
    
    * Update README.md
    
    * Added 2019 data and reorganized
    
    * Update mask.geojson
    
    * Added 2019 data and updated README with procedure
    
    * Updated data and reformat map
    
    * Log of total value of tufts land
    
    * Slightly recenter map

--- main.js
+++ main.js
@@ -8,31 +8,31 @@ function color(owner) {
   }
 }
 
-var width = 1000,
-    height = 1000;
+var height = 1000,
+    width = 2000;
 
 var svg = d3.select("div#map").append("svg")
             .attr("width", "100%")
             .attr("height", "100%")
-            .attr('viewBox', `0 0 ${height} ${width}`)
+            .attr('viewBox', `0 0 ${width} ${height}`)
             .attr('preserveAspectRatio', 'xMinYMid');
 
 var projection = d3.geoAlbers()
-    .rotate([71.12,0])
-    .center([0, 42.406])
-    .scale(4200000)
+    .rotate([71.118,0])
+    .center([0, 42.4067])
+    .scale(5700000)
     .translate([width / 2, height / 2]);
 
 var path = d3.geoPath()
     .projection(projection);
 
 Promise.all([
-  d3.json("data/tufts-medford.geojson"),
-  d3.json("data/tufts-somerville.geojson"),
+  d3.json("data/2019/tufts-medford.json"),
+  d3.json("data/2019/tufts-somerville.json"),
 ]).then(render);
 
 function render(data) {
-  map = [].concat.apply([], data.map(x => x.features))
+  map = [].concat.apply([], data.map(x => x.features));
   svg.append("g")
     .selectAll("path")
     .data(map)
@@ -40,4 +40,9 @@ function render(data) {
       .attr("stroke", "black")
       .attr("fill", function (d) {return color(d.properties.OWNER1)})
       .attr("d", path);
+
+  console.log(map
+    .filter(x => color(x.properties.OWNER1) != "none")
+    .map(x => (x.properties.TOTAL_VAL))
+    .reduce((x, y) => x + y));
 }
